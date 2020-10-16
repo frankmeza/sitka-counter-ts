fa88028210dde2034ec46f0c8c7f8f1531b30733
Frank Meza <frankmezapdx@gmail.com>
2020-10-15 23:18:17 -0700

    add state: donation, tip

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,21 +1,24 @@
 import { Sitka } from "olio-sitka";
 import { Store } from "redux";
 
-import {
-    CounterModule,
-    CounterState,
-} from "./counter";
+import { CounterModule, CounterState } from "./counter";
+import { DonationModule, DonationState } from "./donation";
+import { TipModule, TipState } from "./tip";
 
 export interface AppModules {
     readonly counter: CounterModule;
+    readonly donation: DonationModule;
+    readonly tip: TipModule;
 }
 
 export interface AppState {
     readonly counter: CounterState;
+    readonly donation: DonationState;
+    readonly tip: TipState;
     readonly __sitka__: Sitka<AppModules>;
 }
 
 const sitka = new Sitka<AppModules>();
-sitka.register([new CounterModule()]);
+sitka.register([new CounterModule(), new DonationModule(), new TipModule()]);
 
 export const store = <Store>sitka.createStore();

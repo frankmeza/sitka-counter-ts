d1f707622ee1c73d1f288f906f0fd03dfaf54183
Frank Meza <frankmezapdx@gmail.com>
2020-10-15 23:20:23 -0700

    integrate new modules, components

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,70 @@ import logo from "./logo.svg";
 import { connect } from "react-redux";
 import { AppState, AppModules } from "./modules";
 import { CounterModule } from "./modules/counter";
+import { DonationModule } from "./modules/donation";
 
-interface AppProps {
-    readonly counterModule: CounterModule;
+import DonationField from "./components/donation";
+import TipField from "./components/tip";
+
+import { generatePercentageTipOptions } from "./constants";
+
+type AppProps = {
     readonly sitkaState: AppState;
-}
+    readonly counterModule: CounterModule;
+    readonly donationModule: DonationModule;
+};
+
+const DONATION_AMOUNT = "DONATION AMOUNT: ";
+const TIP_AMOUNT = "TIP AMOUNT: ";
+const FORM_HEADER = "Donation and Tip Form, and Summary";
+const NO_DONATION = "NO DONATION";
+const SUBMIT = "submit donation";
 
 const App = (props: AppProps) => {
-    const { counterModule, sitkaState } = props;
-    const { counter } = sitkaState;
+    const {
+        // app state
+        sitkaState: { donation, tip },
 
-    const { handleDecrement, handleIncrement } = counterModule;
+        // sitka modules
+        donationModule: { handleSetDonation },
+    } = props;
+
+    const subHeaderTextDonation =
+        donation ? `${DONATION_AMOUNT} ${donation}` :
+        `${NO_DONATION}`;
+
+    const subHeaderTextTip =
+        donation ? `${TIP_AMOUNT} ${tip.amount}` :
+        "";
+
+    const subHeaderTextTotal = donation + tip.amount;
+    const tipOptions = generatePercentageTipOptions(donation);
+
+    const setDonation = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { target } = event;
+        handleSetDonation(Number(target.value));
+    };
 
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">
-                    Welcome to Sitka Counter {counter}
-                </h1>
+                <h1 className="App-title">{FORM_HEADER}</h1>
+                <h3>{subHeaderTextDonation}</h3>
+                <h3>{subHeaderTextTip}</h3>
+                <h3>{subHeaderTextTotal}</h3>
             </header>
 
-            <button onClick={handleDecrement}>DECREMENT</button>
-            <button onClick={handleIncrement}>INCREMENT</button>
+            <DonationField
+                donationAmount={donation}
+                setDonation={setDonation}
+            />
+
+            <TipField options={tipOptions} />
+
+            <button type="submit" name="submitButton">
+                {SUBMIT}
+            </button>
         </div>
     );
 };
@@ -36,7 +77,8 @@ export default connect((state: AppState) => {
     const modules: AppModules = state.__sitka__.getModules();
 
     return {
-        counterModule: modules.counter,
         sitkaState: state,
+        counterModule: modules.counter,
+        donationModule: modules.donation,
     };
 })(App);

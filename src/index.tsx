f79e7434c989289a5777562b4cca13d1228962a5
Frank Meza <frankmezapdx@gmail.com>
2020-10-15 23:18:58 -0700

    formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
-import App from "./App"
-import "./index.css"
-import registerServiceWorker from "./registerServiceWorker"
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-import { Provider } from "react-redux"
-import { store } from "./sitka/index"
+import { Provider } from "react-redux";
+import { store } from "./modules/index";
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>,
-    document.getElementById("root")
-)
+    document.getElementById("root"),
+);
 
-registerServiceWorker()
+registerServiceWorker();
